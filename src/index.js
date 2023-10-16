a95d438dd55695a49d1c5189807eb49cdafd5cce
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-10-16 14:57:17 +0100

    used tenary operator to render JSX conditionally

--- src/index.js
+++ src/index.js
@@ -68,27 +68,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  //const pizzas=[]
+  const numPizza = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
-
-      {/* <Pizza
-        photoName="pizzas/prosciutto.jpg"
-        name="Pizza Prosciutto"
-        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        price={12}
-      />
-       <Pizza
-        photoName="pizzas/salamino.jpg"
-        name="Pizza salamino"
-        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        price={67}
-      /> */}
+      {numPizza > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We are Still Working on our menu. Please check back later</p>
+      )}
     </main>
   );
 }
@@ -106,9 +100,26 @@ function Pizza(props) {
   );
 }
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We are Open
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We are Open until {closeHour}:00. Come and visit us or order online
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We are happy to have you between {openHour} and {closeHour}
+        </p>
+      )}
+      .
     </footer>
   );
 }
